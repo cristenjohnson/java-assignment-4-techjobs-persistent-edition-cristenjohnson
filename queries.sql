894592e94ff7890ce41f6650e5b63078cf122062
Brownie <cristenjohnson@rocketmail.com>
2023-02-24 14:52:14 -0600

    added queries

--- queries.sql
+++ queries.sql
@@ -1,7 +1,13 @@
 -- Part 1: Test it with SQL
 
 -- Part 2: Test it with SQL
+SELECT * FROM techjobs.employer;
+WHERE location;
+IN(SELECT techjobs FROM employer WHERE location='St. Louis');
 
 -- Part 3: Test it with SQL
+DROP TABLE techjobs.job;
 
 -- Part 4: Test it with SQL
+SELECT * FROM techjobs.skill;
+ORDER BY skill ASC;
